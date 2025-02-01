6cb64dc1c85ad8fbec06b475216db8c5ec79ae23
hostem17 <vjjais17@gmail.com>
2025-02-02 00:10:05 +0530

    base routes and layout done

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,30 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Route, Routes } from "react-router";
+import Home from "./routes/Home";
+import User from "./routes/User";
+import Auth from "./routes/Auth";
+
+import Expenses from "./routes/Expenses";
+
+import Layout from "./components/Layout";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <p className="read-the-docs">
-        
-      </p>
+      <Routes>
+        <Route index element={<Home />} />
+
+        <Route path ="auth" element ={<Auth/>}/>
+
+        <Route element={<Layout />}>
+          <Route path="user" element={<User />} />
+          <Route path="expenses" element={<Expenses />} />
+        </Route>
+      </Routes>
     </>
   );
 }
