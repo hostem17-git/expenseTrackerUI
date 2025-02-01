7d1955df74175a4d80d564313edb10ee39f9ca52
hostem17 <vjjais17@gmail.com>
2025-02-02 02:05:30 +0530

    home page done

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { NavLink } from "react-router";
 
 function Home() {
-  return <div>HomePage</div>;
-}
+  return (
+    <div className="bg-[url(/homeBG.png)] w-full h-dvh flex flex-col items-center justify-around">
+      <div className="flex flex-col items-center">
+        <div className="text-7xl font-bold text-[#25D366]">
+          Whats
+          <span className="text-white drop-shadow-[0_0_2px_#25D366]">
+            Pense
+          </span>
+        </div>
+        <div className="text-xl mt-10 test"> Expense tracking made easy!</div>
+      </div>
 
+      <NavLink to="/auth">
+        <button className="cursor-pointer p-2 rounded-full font-semibold text-lg text-white bg-green-400  hover:outline hover:text-green-400 hover:bg-white">
+          Get started!
+        </button>
+      </NavLink>
+    </div>
+  );
+}
 export default Home;
