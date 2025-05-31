a34562733a6d9dde788ed405d44f5af5da29ec89
hostem17-git <vjjais17@gmail.com>
2025-05-31 19:28:26 +0530

    baseURL changed

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
-  // baseURL: "https://expensetracker-u51b.onrender.com/api/v1",
+  // baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://expensetracker-u51b.onrender.com/api/v1",
   withCredentials: true,
 });
 
@@ -12,7 +12,7 @@ apiRequest.interceptors.response.use(
   },
   (error) => {
     if (error.response && (error.response.status === 401 || error.response.status === 403)) {
-      
+
       window.location.href = "/";
     }
     return Promise.reject(error);
